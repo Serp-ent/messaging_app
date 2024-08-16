dbe78baa32c55cea4041adbc09a057f09f170471
kenny <kenny909@mat.umk.pl>
2024-08-17 00:13:16 +0200

    Add menu sliding from left side

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
 import styles from './Home.module.css'
 
 export default function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <div className={styles.mainContainer}>
-      <aside className={styles.sidebarLeft}>
-
+      <aside className={`${styles.sidebarLeft} ${isMenuOpen ? styles.open : ''}`}>
+        <ul>
+          <li><a href="">Contact1</a></li>
+          <li><a href="">Contact2</a></li>
+          <li><a href="">Contact3</a></li>
+          <li><a href="">Contact4</a></li>
+        </ul>
       </aside>
-      <main className={styles.main}>
 
+      <main className={styles.main}>
+        <h1>Main content</h1>
       </main>
+
       <aside className={styles.sidebarRight}>
 
       </aside>
 
+      <button className={styles.menuButton} onClick={toggleMenu}>
+        &#9776;
+      </button>
+      {isMenuOpen && <div className={styles.overlay} onClick={toggleMenu}></div>}
     </div>
   );
 }
